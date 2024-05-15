806ab2a1402a238b54558d090d3af3344c67e47a
Écio Ferraz <ecioferraz@gmail.com>
2024-05-15 12:39:13 -0300

    feat: 02 ex 03

---
+++ Aula01/src/main/kotlin/com/betrybe/aula02/Ex03.kt
@@ -0,0 +1,13 @@
+package com.betrybe.aula02
+
+fun main() {
+    print("Digite o primeiro número: ")
+    val num1 = readln().toDouble()
+
+    print("Digite o segundo número: ")
+    val num2 = readln().toDouble()
+
+    println("O número maior é ${getBiggerNum(num1, num2)}")
+}
+
+fun getBiggerNum(num1: Double, num2: Double) = if (num1 > num2) num1 else num2
