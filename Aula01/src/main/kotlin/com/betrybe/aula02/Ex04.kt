d584d39c4cf5529f337b5e7b7a2a3829c7e31899
Écio Ferraz <ecioferraz@gmail.com>
2024-05-15 12:48:03 -0300

    feat: 02 ex 04

---
+++ Aula01/src/main/kotlin/com/betrybe/aula02/Ex04.kt
@@ -0,0 +1,24 @@
+package com.betrybe.aula02
+
+fun main() {
+    print("Digite a primeira nota: ")
+    val grade1 = readln().toDouble()
+
+    print("Digite a segunda nota: ")
+    val grade2 = readln().toDouble()
+
+    print("Digite a terceira nota: ")
+    val grade3 = readln().toDouble()
+
+    val average = getAverage(grade1, grade2, grade3)
+
+    if (average >= 0 && average < 3) {
+        println("REPROVADO")
+    } else if (average >= 3 && average < 7) {
+        println("EXAME")
+    } else {
+        println("APROVADO")
+    }
+}
+
+fun getAverage(grade1: Double, grade2: Double, grade3: Double) = (grade1 + grade2 + grade3) / 3
