32f4e5dfc72e8b254766ecc508779c78c145551e
Écio Ferraz <ecioferraz@gmail.com>
2024-06-20 16:31:17 -0300

    feat: aula 4

---
+++ Orientacao-a-Objetos-com-Kotlin/src/main/kotlin/com/betrybe/aula04/App.kt
@@ -0,0 +1,52 @@
+package com.betrybe.aula04
+
+abstract class Animal {
+    abstract val race: String
+
+    open fun eat () {
+        println("Ate!")
+    }
+}
+
+class Rabbit: Animal() {
+    override val race: String
+        get() = TODO("Not yet implemented")
+
+    override fun eat() {
+        TODO("Not yet implemented")
+    }
+}
+
+class Cat: Animal() {
+    override val race: String
+        get() = TODO("Not yet implemented")
+}
+
+class Person (val name: String, var age: Int) {
+
+    fun printName() {
+        println(name)
+    }
+
+    fun printAge() {
+        println(age)
+    }
+
+    fun ageUp() {
+        age += 1
+    }
+}
+
+fun main () {
+    val me = Person("Écio Ferraz", 32)
+
+    println(me.name)
+    println(me.age)
+
+    me.printAge()
+    me.printName()
+
+    me.ageUp()
+
+    me.printAge()
+}
---
+++ Orientacao-a-Objetos-com-Kotlin/src/main/kotlin/com/betrybe/aula04/Main.kt
@@ -0,0 +1,33 @@
+package com.betrybe.aula04
+
+import kotlin.math.pow
+
+interface Form2D {
+//    fun calcArea (): Double
+
+    var area: Double
+}
+
+class Rectangle(val width: Double, val height: Double): Form2D {
+    //    override fun calcArea(): Double = width * height
+
+    override var area: Double
+        get() = width * height
+        set(value) {}
+}
+
+class Circle(val radius: Double): Form2D {
+    //    override fun calcArea(): Double = Math.PI * radius.pow(2.0)
+
+    override var area: Double
+        get() = Math.PI * radius.pow(2.0)
+        set(value) {}
+}
+
+fun main () {
+    val rectangleArea = Rectangle(3.0, 5.0)
+    val circleArea = Circle(10.0)
+
+    println(rectangleArea.area)
+    println(circleArea.area)
+}
